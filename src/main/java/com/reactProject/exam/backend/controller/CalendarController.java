ead0ad61821a9cad97a5a1255c0d4e2e29698ac8
ligaemuya <96481698+ligaemuya@users.noreply.github.com>
2024-11-14 21:52:18 +0900

    스웨거 설명추가

--- src/main/java/com/reactProject/exam/backend/controller/CalendarController.java
+++ src/main/java/com/reactProject/exam/backend/controller/CalendarController.java
@@ -3,6 +3,12 @@ package com.reactProject.exam.backend.controller;
 import com.reactProject.exam.backend.Entity.CalendarEntity;
 import com.reactProject.exam.backend.dto.CalendarDto;
 import com.reactProject.exam.backend.service.CalendarService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -17,21 +23,46 @@ public class CalendarController {
     @Autowired
     private CalendarService calendarService;
 
+    @Operation(summary = "일정 생성", description = "새로운 캘린더 일정 생성")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "일정 생성 성공", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = CalendarDto.class)) }),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청"),
+            @ApiResponse(responseCode = "500", description = "서버 오류")
+    })
     @PostMapping("/insert")
-    public CalendarDto createEvent(@RequestBody CalendarDto calendarDto) {
+    public CalendarDto createEvent(@Parameter(description = "생성할 이벤트 정보") @RequestBody CalendarDto calendarDto) {
         return calendarService.saveEvent(calendarDto);
     }
 
+    @Operation(summary = "전체 일정 조회", description = "모든 일정을 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "이벤트 조회 성공", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = List.class)) }),
+            @ApiResponse(responseCode = "500", description = "서버 오류")
+    })
     @GetMapping("/select")
     public List<CalendarDto> getAllEvents() {
         return calendarService.getAllEvents();
     }
 
+    @Operation(summary = "일정 수정", description = "기존 일정 수정")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "수정 성공", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = CalendarDto.class)) }),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청"),
+            @ApiResponse(responseCode = "404", description = "이벤트를 찾을 수 없음"),
+            @ApiResponse(responseCode = "500", description = "서버 오류")
+    })
     @PutMapping("/update")
-    public CalendarDto updateEvent(@RequestBody CalendarDto calendarDto) {
+    public CalendarDto updateEvent(@Parameter(description = "업데이트할 일정 정보") @RequestBody CalendarDto calendarDto) {
         return calendarService.updateEvent(calendarDto.getId(), calendarDto);
     }
 
+    @Operation(summary = "일정 삭제", description = "기존 일정을 삭제합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "삭제 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청"),
+            @ApiResponse(responseCode = "404", description = "일정을 찾을 수 없음"),
+            @ApiResponse(responseCode = "500", description = "서버 오류")
+    })
     @DeleteMapping("/delete")
     public void deleteEvent(@RequestBody CalendarDto calendarDto) {
         calendarService.deleteEvent(calendarDto.getId());
--- src/main/java/com/reactProject/exam/backend/controller/UserController.java
+++ src/main/java/com/reactProject/exam/backend/controller/UserController.java
@@ -5,6 +5,10 @@ import com.reactProject.exam.backend.dto.LoginDto;
 import com.reactProject.exam.backend.dto.SessionDto;
 import com.reactProject.exam.backend.dto.SignUpDto;
 import com.reactProject.exam.backend.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,19 +19,37 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Operation(summary = "회원가입", description = "이름, ID, 패스워드를 작성하여 회원가입")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "회원가입 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청"),
+            @ApiResponse(responseCode = "500", description = "서버 오류")
+    })
     @PostMapping("/signup")
-    public String signUp(@RequestBody SignUpDto signUpDto) throws Exception {
+    public String signUp(@Parameter(description = "회원가입 정보")  @RequestBody SignUpDto signUpDto) throws Exception {
        userService.signUp(signUpDto);
        return "회원가입 성공";
     }
 
+    @Operation(summary = "로그인", description = "ID, 패스워드를 입력하여 로그인")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "회원가입 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청"),
+            @ApiResponse(responseCode = "500", description = "서버 오류")
+    })
     @PostMapping("/login")
-    public SessionDto login(@RequestBody LoginDto loginDto) throws Exception {
+    public SessionDto login(@Parameter(description = "계정 정보") @RequestBody LoginDto loginDto) throws Exception {
         return userService.login(loginDto);
     }
 
+    @Operation(summary = "ID중복확인", description = "회원가입시 ID를 입력후 중복확인")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "회원가입 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청"),
+            @ApiResponse(responseCode = "500", description = "서버 오류")
+    })
     @GetMapping("/checkUserId")
-    public CheckUserDto checkUserId(@RequestParam("userId") String userId) {
+    public CheckUserDto checkUserId(@Parameter(description = "가입ID") @RequestParam("userId") String userId) {
         return userService.isUserIdTaken(userId);
     }
     
